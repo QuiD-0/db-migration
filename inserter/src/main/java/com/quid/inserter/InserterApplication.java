a0612908490a87ae8dda00e680f2f1d82248d739
QuiD-0 <wodnd101@naver.com>
2024-05-31 09:33:32 +0900

    init insert server

---
+++ inserter/src/main/java/com/quid/inserter/InserterApplication.java
@@ -0,0 +1,13 @@
+package com.quid.inserter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InserterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InserterApplication.class, args);
+    }
+
+}
---
+++ inserter/src/test/java/com/quid/inserter/InserterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quid.inserter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InserterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
