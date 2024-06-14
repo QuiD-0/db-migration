911ae2af8ade11d2a9d2d38ce0d06d7b1074399d
QuiD-0 <wodnd101@naver.com>
2024-06-14 14:47:41 +0900

    consumer 생성

---
+++ sinker/src/main/kotlin/com/quid/sinker/pay/config/KafkaConfig.kt
@@ -0,0 +1,10 @@
+package com.quid.sinker.pay.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+
+@EnableKafka
+@Configuration
+class KafkaConfig {
+
+}
--- sinker/src/main/kotlin/com/quid/sinker/pay/gateway/consumer/KafkaPayConsumer.kt
+++ sinker/src/main/kotlin/com/quid/sinker/pay/gateway/consumer/KafkaPayConsumer.kt
@@ -1,7 +1,14 @@
 package com.quid.sinker.pay.gateway.consumer
 
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
 class KafkaPayConsumer {
-    fun consume() {
-        println("KafkaPayConsumer")
+
+    @KafkaListener(topics = ["mysql.old_db.PAYMENT"])
+    fun consume(message: String, ack: Acknowledgment) {
+        println("Consumed message: $message")
     }
 }
